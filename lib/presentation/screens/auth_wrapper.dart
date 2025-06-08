3c1cf5d52a9b67dea43929f61ea20a6c98be4f97
Arlachov Daniil <105928091+freeztyl00@users.noreply.github.com>
2025-06-08 13:36:06 +0200

    Revert "stable version with only loader and session controller "

--- lib/presentation/screens/auth_wrapper.dart
+++ lib/presentation/screens/auth_wrapper.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import '../../presentation/screens/home_screen.dart';
 import '../../presentation/screens/login_screens.dart';
-import 'biometric_auth_screen.dart';
+import '../../presentation/screens/setup_screen.dart';
+import '../providers/transaction_provider.dart';
 
 // Віджет, що перевіряє стан автентифікації користувача
 class AuthWrapper extends StatelessWidget {
@@ -24,8 +27,25 @@ class AuthWrapper extends StatelessWidget {
           return const LoginScreen();
         }
 
-        // Якщо користувач залогінений, просимо підтвердити вхід біометрією
-        return const BiometricAuthScreen();
+        final provider = Provider.of<TransactionProvider>(context, listen: false);
+        provider.updateUser(user.uid);
+        return FutureBuilder(
+          future: provider.loadData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // Завантаження даних користувача
+              return const Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              );
+            }
+            // Якщо баланс ще не встановлено - показуємо екран налаштування
+            if (provider.initialBalance == null) {
+              return const SetupScreen();
+            }
+            // Інакше переходимо на головний екран
+            return const HomeScreen();
+          },
+        );
       },
     );
   }
